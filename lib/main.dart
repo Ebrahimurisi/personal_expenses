a185d1c986fb13a530bd237cda8c932ef7b9c764
wajdynasser <wanassermohammed@gmail.com>
2024-10-06 22:10:37 +0300

    the new edite

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:personal_expenses/presentation/screens/onboarding.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-    body: Column(
-      children: [
-
-          Text("Wajdy"),
-          Lottie.network('https://lottie.host/3fca1209-09ce-4374-b4b1-ad51230cd881/3VvKtFZ9x8.json'),
-
-      ],
-    ),
-    ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    home: Onboarding(),
     );
   }
 }
--- lib/presentation/screens/onboarding.dart
+++ lib/presentation/screens/onboarding.dart
@@ -1,1 +1,28 @@
-import '';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+
+            const Text("Wajdy"),
+            Lottie.network('https://lottie.host/5b763c50-5709-4096-9a8e-81be41766778/ySi8Q4xSbB.json'),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
